5bf6fb4b8a44737775ceb3e79b4f3dcb5ff3ab3b
Barsil Nzola <barsiljohn@gmail.com>
2020-06-26 22:18:36 +0300

    added logic to save shift and ran test

--- src/main/java/models/CaesarCipher.java
+++ src/main/java/models/CaesarCipher.java
@@ -3,12 +3,18 @@ package models;
 public class CaesarCipher {
 
     private String letter;
+    private int shift;
 
     public CaesarCipher(String letter, int shift) {
         this.letter = letter;
+        this.shift = shift;
     }
 
     public String getLetter() {
         return this.letter;
+    }
+
+    public int getShift() {
+        return this.shift;
     }
 }
