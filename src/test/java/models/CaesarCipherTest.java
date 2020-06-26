8d84d794e9e7a89e8f4a5fb2378498a94c01ce5c
Barsil Nzola <barsiljohn@gmail.com>
2020-06-26 22:12:35 +0300

    ran test on getting shift inputted

--- src/test/java/models/CaesarCipherTest.java
+++ src/test/java/models/CaesarCipherTest.java
@@ -6,14 +6,20 @@ import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
     @Test
-    public void newCaesarCipher_instantiatesCorrectly () {
-        CaesarCipher testEncryption = new CaesarCipher ( "a", 1);
+    public void newCaesarCipher_instantiatesCorrectly() {
+        CaesarCipher testEncryption = new CaesarCipher("a", 1);
         assertEquals(true, testEncryption instanceof CaesarCipher);
     }
 
     @Test
-    public void newCaesarCipher_getsLetter_a () {
-        CaesarCipher testEncryption = new CaesarCipher ( "a", 1);
+    public void newCaesarCipher_getsLetter_a() {
+        CaesarCipher testEncryption = new CaesarCipher("a", 1);
         assertEquals('a', testEncryption.getLetter());
+    }
+
+    @Test
+    public void newCaesarCipher_getsShift() {
+        CaesarCipher testEncryption = new CaesarCipher("a", 1);
+        assertEquals(1, testEncryption.getShift());
     }
 }
