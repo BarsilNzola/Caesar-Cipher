e966ac92e81095156b5988b2f3b845d0540efffa
Barsil Nzola <barsiljohn@gmail.com>
2020-06-27 12:23:12 +0300

    added logic to decrypt the encrypted text

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ import java.io.Console;
 
 public class App {
     public static void main(String[] args) {
+        // Input for Encryption.
         Console myConsole = System.console();
         System.out.println("Enter the text to encrypt");
         String inputtedText = myConsole.readLine();
@@ -14,5 +15,17 @@ public class App {
         CaesarCipher createdCaesarCipher = new CaesarCipher(inputtedText, encryptionKey);
         String encrypted = createdCaesarCipher.cipherEncryption();
         System.out.println("ENCRYPTION:" + encrypted);
+        System.out.println("-----------------");
+
+        // Input for Decryption.
+        System.out.println("Enter Encrypted Text");
+        String encryptedText = myConsole.readLine();
+        System.out.println("Enter shift key");
+        String decryptShiftKey = myConsole.readLine();
+        int decryptionKey = Integer.parseInt(decryptShiftKey);
+        CaesarCipher createdCaesarCipher_2 = new CaesarCipher(encryptedText, decryptionKey);
+        String decrypted = createdCaesarCipher_2.cipherDecryption();
+        System.out.println("DECRYPTION:" + decrypted);
+        System.out.println("------------------");
     }
 }
